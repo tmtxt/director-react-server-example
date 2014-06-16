7ba3752a0927c351a9767538705624dde6c54fa0
tmtxt <tmtxt@tmtxt-mac.local>
2014-06-16 11:04:51 +0700

    update gulp task, not use gulp-browserify, use browserify directly

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
 var react = require('gulp-react');
 var plumber = require('gulp-plumber');
-var browserify = require('gulp-browserify');
+var browserify = require('browserify');
+var source = require("vinyl-source-stream");
+var clean = require('gulp-clean');
 
+// Transform jsx files to js files for Tern to show suggestion
 gulp.task('jsx', function(){
   gulp.src(['main.jsx', 'test1.jsx', 'test2.jsx'])
     .pipe(plumber())
@@ -10,42 +13,56 @@ gulp.task('jsx', function(){
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('browserify', function(){
-  gulp.src('main.js')
-    .pipe(plumber())
-    .pipe(browserify({
-      basedir: './'
-    }))
-    .on('prebundle', function(bundle){
-      bundle.external('react');
-      bundle.require('./main.js');
-    })
-    .pipe(gulp.dest('public/javascripts'));
-  
-  gulp.src('test1.js')
-    .pipe(plumber())
-    .pipe(browserify({
-      basedir: './'
-    }))
-    .on('prebundle', function(bundle){
-      bundle.external('react');
-      bundle.require('./test1.js');
-    })
-    .pipe(gulp.dest('public/javascripts'));
+// Browserify npm packages
+gulp.task('browserify-lib', function(){
+  var b = browserify();
+  b.require('react');
+  b.bundle()
+    .pipe(source('react.js'))
+    .pipe(gulp.dest('./public/javascripts'));
 
-  gulp.src('test2.js')
-    .pipe(plumber())
-    .pipe(browserify({
-      basedir: './'
-    }))
-    .on('prebundle', function(bundle){
-      bundle.external('react');
-      bundle.require('./test2.js');
-    })
-    .pipe(gulp.dest('public/javascripts'));
+  b = browserify();
+  b.require('director');
+  b.bundle()
+    .pipe(source('director.js'))
+    .pipe(gulp.dest('./public/javascripts'));
 });
 
+// Browserify modules
+gulp.task('browserify-modules', ['jsx'], function(){
+  var b = browserify();
+  b.add('./main.js');
+  b.external(require.resolve('react', {expose: 'react'}));
+  b.require('./main.js');
+  b.bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('./public/javascripts/'));
+
+  b = browserify();
+  b.add('./test1.js');
+  b.external(require.resolve('react', {expose: 'react'}));
+  b.require('./test1.js');
+  b.bundle()
+    .pipe(source('test1.js'))
+    .pipe(gulp.dest('./public/javascripts/'));
+
+  b = browserify();
+  b.add('./test2.js');
+  b.external(require.resolve('react', {expose: 'react'}));
+  b.require('./test2.js');
+  b.bundle()
+    .pipe(source('test2.js'))
+    .pipe(gulp.dest('./public/javascripts/'));
+});
+
+// Clean
+gulp.task('clean', function(){
+  return gulp.src(['main.js', 'test1.js', 'test2.js'], {read: false})
+  .pipe(clean());
+});
+
+// Watching
 gulp.task('watch', function(){
   gulp.watch(['main.jsx', 'test1.jsx', 'test2.jsx'], ['jsx']);
-  gulp.watch(['main.js', 'test1.js', 'test2.js'], ['browserify']);
+  gulp.watch(['main.js', 'test1.js', 'test2.js'], ['browserify-modules']);
 });
